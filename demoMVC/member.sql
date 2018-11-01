e6714406f44ed028ebdcb5faf608bd55ba4b57bc
QUYENNguyenphuong <quyen.nguyen@ceresolutions.com>
2018-11-01 10:12:28 +0700

    alter table member

--- demoMVC/member.sql
+++ demoMVC/member.sql
@@ -25,9 +25,10 @@ CREATE TABLE `member`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(100)  NULL DEFAULT NULL,
   `password` varchar(100) NULL DEFAULT NULL,
-  `level` varchar(20) NULL DEFAULT NULL,
+  `level` int(20) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB  CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ;
 SET FOREIGN_KEY_CHECKS = 1;
 
-ALTER TABLE `member` 	ADD COLUMN email varchar(100) NULL DEFAULT NULL;
+ALTER TABLE `member` 	ADD COLUMN email varchar(100) NULL DEFAULT NULL,
+  MODIFY `level` varchar(20) NULL DEFAULT NULL
